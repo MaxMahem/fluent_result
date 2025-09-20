6d30404201aa477a67a61e54365a0de1bcf7a9cb
MaxMahem <maxtmahem@gmail.com>
2025-09-20 08:02:58 -0500

    kill target

--- target/debug/deps/ok_or-735741db3be364cc.d
+++
@@ -1,9 +0,0 @@
-C:\Users\maxma\source\repos\Rust\ok_or\target\debug\deps\ok_or-735741db3be364cc.d: src\lib.rs src\into_err.rs src\into_ok.rs src\ok_or_panic.rs src\unwrap_never.rs
-
-C:\Users\maxma\source\repos\Rust\ok_or\target\debug\deps\libok_or-735741db3be364cc.rmeta: src\lib.rs src\into_err.rs src\into_ok.rs src\ok_or_panic.rs src\unwrap_never.rs
-
-src\lib.rs:
-src\into_err.rs:
-src\into_ok.rs:
-src\ok_or_panic.rs:
-src\unwrap_never.rs:
--- target/debug/deps/result_utils-10b6830798f144fb.d
+++
@@ -1,9 +0,0 @@
-C:\Users\maxma\source\repos\Rust\ok_or\target\debug\deps\result_utils-10b6830798f144fb.d: src\lib.rs src\into_err.rs src\into_ok.rs src\ok_or_panic.rs src\unwrap_never.rs
-
-C:\Users\maxma\source\repos\Rust\ok_or\target\debug\deps\libresult_utils-10b6830798f144fb.rmeta: src\lib.rs src\into_err.rs src\into_ok.rs src\ok_or_panic.rs src\unwrap_never.rs
-
-src\lib.rs:
-src\into_err.rs:
-src\into_ok.rs:
-src\ok_or_panic.rs:
-src\unwrap_never.rs:
